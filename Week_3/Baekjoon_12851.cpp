bc72d1197bbf55f088289c6d400afaab7f9549c7
taebee00 <taebee0217@gmail.com>
2023-04-09 18:43:02 +0900

    2023_04_09_2

--- Week_3/Baekjoon_12851.cpp
+++ Week_3/Baekjoon_12851.cpp
@@ -1,35 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n,k, visitied[100001]={0,};
+int n,k, visitied[100001]={0,}, way[100001]={0,};
 queue<int> mp;
 
 pair<int,int> find(int loc, int goal){
     visitied[loc]=1;
+    way[loc]=1;
     mp.push(loc);
+    int flag,mn=100000,cnt=0;
     while(!mp.empty()){
-        int mn,cnt=0;
         if (mp.front()==goal){
-            mn=visitied[mp.front()];
-            while(!mp.empty()){
-                if (mn==visitied[mp.front()]){
-                    cnt++;
-                }
-                mp.pop();
+            return {mn,way[goal]};
+        }
+        if (mp.front()>0){
+            if (!visitied[mp.front()-1]){
+                mp.push(mp.front()-1);
+                visitied[mp.front()-1]=visitied[mp.front()]+1;
+                way[mp.front()-1]=way[mp.front()];
             }
-            return {mn,cnt};
+            else if (visitied[mp.front()-1]==visitied[mp.front()]+1){
+                way[mp.front()-1]+=way[mp.front()];
+            }
         }
-        if (mp.front()>0&&!visitied[mp.front()-1]){
-            mp.push(mp.front()-1);
-            visitied[mp.front()-1]=visitied[mp.front()]+1;
+        if (mp.front()<100000){
+            if (!visitied[mp.front()+1]){
+                mp.push(mp.front()+1);
+                visitied[mp.front()+1]=visitied[mp.front()]+1;
+                way[mp.front()+1]=way[mp.front()];
+            }
+            else if (visitied[mp.front()+1]==visitied[mp.front()]+1){
+                way[mp.front()+1]+=way[mp.front()];
+            }
         }
-        if (mp.front()<100000&&!visitied[mp.front()+1]){
-            mp.push(mp.front()-1);
-            visitied[mp.front()+1]=visitied[mp.front()]+1;
-        }
-        if (mp.front()*2<=100000&&!visitied[mp.front()*2]){
-            mp.push(mp.front()*2);
-            visitied[mp.front()*2]=visitied[mp.front()]+1;
+        if (mp.front()*2<=100000){
+            if (!visitied[mp.front()*2]){
+                mp.push(mp.front()*2);
+                visitied[mp.front()*2]=visitied[mp.front()]+1;
+                way[mp.front()*2]=way[mp.front()];
+            }
+            else if (visitied[mp.front()*2]==visitied[mp.front()]+1){
+                way[mp.front()*2]+=way[mp.front()];
+            }
         }
         mp.pop();
     }
@@ -39,5 +51,5 @@ int main()
 {
     cin >> n >> k;
     pair<int,int> ret = find(n,k);
-    cout << ret.first << ret.second;
+    cout << ret.first-1 << " " << ret.second;
 }
