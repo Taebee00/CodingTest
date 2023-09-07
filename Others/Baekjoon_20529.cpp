c64bdbc741e67580c644407f96f3f412ad1cea56
Joe <taebee0217@gmail.com>
2023-09-08 00:41:43 +0900

    2023_09_07

--- Others/Baekjoon_20529.cpp
+++ Others/Baekjoon_20529.cpp
@@ -3,77 +3,63 @@
 using namespace std;
 
 int T,N,mn=INT_MAX;
-int mp[100]={0,};
+char mbti_arr[4]={'E','S','T','J'};
+int mp[32];
 vector<int> v;
-vector<int> choose_v;
 
-int cal(){
+// void bin(int n){
+//     vector<int> bin_v;
+//     while(n){
+//         bin_v.push_back(n%2);
+//         n/=2;
+//     }
+//     for (int i=bin_v.size()-1;i>=0;i--){
+//         printf("%d",bin_v[i]);
+//     }
+//     printf(" ");
+// }
+
+int cal(int i,int j){
     int sum=0;
-    for (int i=0;i<2;i++){
-        for (int j=i+1;j<3;j++){
-            int num=v[choose_v[i]]^v[choose_v[j]];
-            
-            for (int k=0;k<4;k++){
-                if ((num&(1<<k))){
-                    sum++;
-                }
-            }
-        }
+    int dist=i^j;
+    //bin(dist);
+    for (int i=0;i<4;i++){
+        if (dist&(1<<i)) sum++;
     }
     return sum;
 }
 
-void mbti(int sz,int choice){
-    if (sz==3){
-        mn=min(cal(),mn);
-    }
-    for (int i=choice+1;i<v.size();i++){
-        choose_v.push_back(i);
-        mbti(sz+1,i);
-        choose_v.pop_back();
-    }
-}
+
+
 
 int main()
 {
     cin >> T;
     for (int i=0;i<T;i++){
-        mn=INT_MAX;
-        string str;
-        cin >> N;
         v.clear();
-        choose_v.clear();
-        int flag=0;
+        cin >> N; 
         for (int j=0;j<N;j++){
+            string str;
+            int temp=0;
             cin >> str;
-            int num=0;
-            if (str[0]=='E'){
-                num|=1<<3;
+            for (int i=0;i<4;i++){
+                if (str[i]==mbti_arr[i]){
+                    temp|=1<<3-i;
+                }
             }
-            if (str[1]=='S'){
-                num|=1<<2;
-            }
-            if (str[2]=='T'){
-                num|=1<<1;
-            }
-            if (str[3]=='J'){
-                num|=1<<0;
-            }
-            if (mp[num]<=2){
-                mp[num]+=1;
-                v.push_back(num);
-            }
-            else{
-                flag=1;
-                break;
+            if (mp[temp]<3){
+                mp[temp]++;
+                v.push_back(temp);
+            }           
+        }
+        int ret=INT_MAX;
+        for (int j=0;j<v.size();j++){
+            for (int k=j+1;k<v.size();k++){
+                for (int l=k+1;l<v.size();l++){
+                    ret=min(ret,cal(v[j],v[k])+cal(v[k],v[l])+cal(v[j],v[l]));
+                }
             }
         }
-        if (!flag){
-            mbti(0,-1);
-            cout << mn << "\n";
-        }
-        else{
-            cout << 0 << "\n";
-        }
+        printf("%d\n",ret);
     }
 }
