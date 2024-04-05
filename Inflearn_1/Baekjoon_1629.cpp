5aca5dcaa8ccbd612fad36a26020547c85c51c1e
joe <taebee0217@gmail.com>
2024-04-06 01:44:45 +0900

    2024_04_06

--- Inflearn_1/Baekjoon_1629.cpp
+++ Inflearn_1/Baekjoon_1629.cpp
@@ -1,37 +1,18 @@
-// #include <bits/stdc++.h>
-// using namespace std;
-
-// long long a, b, c;
-
-// long long multi(long long num) {
-// 	if (num == 1) {
-// 		return a % c;
-// 	}
-// 	long long sum = (multi(num / 2) * multi(num / 2))%c;
-// 	if (num %2) sum*=a%c;
-// 	return sum%c;
-// }
-
-// int main()
-// {
-// 	cin >> a >> b >> c;
-// 	cout << multi(b);
-// }
 #include <bits/stdc++.h>
 
 using namespace std;
 
 long long A,B,C;
 
-int multi(int a){
+long long multi(long long a){
 	if (a==1){
 		return A%C;
 	}
 	else if (a%2==1){
-		return multi(a/2)*multi(a/2)*(A%C)%C;
+		return ((multi(a/2)*multi(a/2))%C*(A%C))%C;
 	}
 	else{
-		return multi(a/2)*multi(a/2)%C;
+		return (multi(a/2)*multi(a/2))%C;
 	}
 }
 
